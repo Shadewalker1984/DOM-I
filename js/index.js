f3455bcab528eb7bee30b2a253ca99d8a25b9ddb
Shadewalker1984 <Shadewalker1984@yahoo.com>
2019-12-04 19:42:48 -0600

    finished the page design, will try to figure out how to add line breaks to the content at the top

--- js/index.js
+++ js/index.js
@@ -51,7 +51,7 @@ links[4].textContent = siteContent.nav["nav-item-5"];
 links[5].textContent = siteContent.nav["nav-item-6"];
 
 let title = document.querySelector('.cta-text');
-title.textContent = siteContent.cta.h1
+title.innerHTML = siteContent.cta.h1;
 title.style.weight = "bold";
 
 
@@ -61,21 +61,33 @@ circleImg.setAttribute('src', siteContent['cta']['img-src']);
 // let btn = document.getElementsbyTagName("button");  
 // btn.innterHRML = siteContent.cta.button           ///
 
-let feat4 = document.querySelector(".text-content h4");
-feat4.textContent = siteContent["main-content"]["features-h4"];
+let feat4 = document.querySelectorAll(".top-content .text-content h4");
+feat4[0].textContent = siteContent["main-content"]["features-h4"];
+feat4[1].textContent = siteContent["main-content"]["about-h4"];
 
-
-let feat4cont = document.querySelector("p");
-feat4cont.textContent = siteContent["main-content"]["features-content"];
-feat4cont.style.width = '50%';
-
-let about4 = document.querySelector('.text-content[1]');
-about4.textContent = siteContent["main-content"]["about-h4"];
+let feat4cont = document.querySelectorAll(".top-content p");
+feat4cont[0].textContent = siteContent["main-content"]["features-content"];
+feat4cont[1].textContent = siteContent["main-content"]["about-content"];
 
 let midImg = document.getElementById("middle-img");
 midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 
+let serv4 = document.querySelectorAll(".bottom-content h4");
+serv4[0].textContent = siteContent["main-content"]["services-h4"];
+serv4[1].textContent = siteContent["main-content"]["product-h4"];
+serv4[2].textContent = siteContent["main-content"]["vision-h4"];
 
-            
-              
+let serv4cont = document.querySelectorAll(".bottom-content p");
+serv4cont[0].textContent = siteContent["main-content"]["services-content"];
+serv4cont[1].textContent = siteContent["main-content"]["product-content"];
+serv4cont[2].textContent = siteContent["main-content"]["vision-content"];
+
+let contEnd = document.querySelector(".contact h4");
+contEnd.textContent = siteContent.contact["contact-h4"];
+
+let contP = document.querySelectorAll(".contact p");
+contP[0].textContent = siteContent.contact.address;
+contP[1].textContent = siteContent.contact.phone;
+contP[2].textContent = siteContent.contact.email;            
+
    
